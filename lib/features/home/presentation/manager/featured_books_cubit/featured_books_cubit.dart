1da9f8ff76a211287db9ff3f75d6721823280b03
Manar <manarhamdikamel@gmail.com>
2023-10-15 20:22:50 +0300

    Create features books method

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../data/Home_repos/home_repo.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksStates>{
+  final HomeRepos homeRepo;
+  FeaturedBooksCubit(super.initialState, this.homeRepo);
+  Future<void> fetchFeaturedBooks()async {
+    emit(FeaturedBooksLoadingState());
+    var result=await homeRepo.fetchNewestBooksMethod();
+    result.fold( (failure) {
+      emit(FeaturedBooksFailureState(failure.errorMess));
+    }
+    ,(books) {
+      emit(FeaturedBooksSuccessState(books));
+        }
+    ) ;
+  }
+
+  }
--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_states.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_states.dart
@@ -1,14 +1,14 @@
 import '../../../data/models/BooksModel.dart';
 
 abstract class FeaturedBooksStates {}
-class FeaturedBooksInitial extends FeaturedBooksStates{}
-class FeaturedBooksLoading extends FeaturedBooksStates{}
-class FeaturedBooksFailure extends FeaturedBooksStates{
+class FeaturedBooksInitialState extends FeaturedBooksStates{}
+class FeaturedBooksLoadingState extends FeaturedBooksStates{}
+class FeaturedBooksFailureState extends FeaturedBooksStates{
    final String errMessage;
-  FeaturedBooksFailure(this.errMessage);
+  FeaturedBooksFailureState(this.errMessage);
 
 }
-class FeaturedBooksSuccess extends FeaturedBooksStates{
+class FeaturedBooksSuccessState extends FeaturedBooksStates{
    final List<BooksModel> books;
-  FeaturedBooksSuccess(this.books);
+  FeaturedBooksSuccessState(this.books);
 }
